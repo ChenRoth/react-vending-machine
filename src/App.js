0a2e276b7c3134930a7802a572e4b2aed2928972
Chen Roth <rothchen@gmail.com>
2020-04-17 11:43:08 +0300

    add bootstrap to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { VendingMachine } from './components/VendingMachine/VendingMachine';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
   render() {
