e686c533f26497caaaea5e9f666592fb9a4818a4
Chen Roth <rothchen@gmail.com>
2020-04-17 12:30:40 +0300

    solve q3 in homework by adding app logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { VendingMachine } from './components/VendingMachine/VendingMachine';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
+  state = {
+    moneyInPocket: 30,
+    products: [],
+  }
   render() {
+    const {products} = this.state;
     return (
       <div>
-        <VendingMachine onBeforeInsert={(coin) => true} onBuy={console.log}/>
+        <VendingMachine onBeforeInsert={this.handleOnBeforeInsert} onBuy={this.handleOnBuy} />
+        {products.map((p, i) =>
+          <img key={i} src={p.img} alt="azov" />
+        )}
       </div>
     )
+  }
+
+  handleOnBuy = (product, change) => {
+    const { moneyInPocket, products } = this.state;
+    this.setState({
+      moneyInPocket: moneyInPocket + change,
+      products: products.concat(product),
+    });
+  }
+
+  handleOnBeforeInsert = (coin) => {
+    const { moneyInPocket } = this.state;
+    const isEnoughMoney = moneyInPocket > coin;
+    if (!isEnoughMoney) {
+      return false;
+    }
+
+    this.setState({
+      moneyInPocket: moneyInPocket - coin,
+    });
+
+    return true;
   }
 }
 
