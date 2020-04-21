3dd0fd4f83a66006d8bd1f341867c36edd0ef68b
Chen Roth <rothchen@gmail.com>
2020-04-21 18:24:02 +0300

    add CardReader component

---
+++ src/components/CardReader/CardReader.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+export class CardReader extends React.Component {
+    state = {
+        num: ''
+    }
+
+    render() {
+        const {num} = this.state;
+        return (
+            <div>
+                <form onSubmit={this.handleOnSwipe}>
+                    <input onChange={this.onChangeNum} value={num} placeholder="type your CC #" maxLength="10" />
+                    <Button type="submit">SWIPE CARD</Button>
+                </form>
+            </div>
+        )
+    }
+
+    onChangeNum = (e) => {
+        const num = e.target.value;
+        this.setState({
+            num,
+        });
+    }
+
+    handleOnSwipe = (e) => {
+        e.preventDefault();
+        const {cards, onInsertCard} = this.props;
+        const {num} = this.state;
+        if (num.length !== 10) {
+            alert('CC # must be 10 digits');
+            return;
+        }
+
+        const insertedCard = cards.find(card => card.num === num);
+        if (!insertedCard) {
+            alert('CC is invalid');
+            return;
+        }
+
+        onInsertCard(insertedCard);
+    }
+}
